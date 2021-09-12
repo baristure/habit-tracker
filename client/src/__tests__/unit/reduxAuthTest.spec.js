a34ab97c761caea5761f555cae565e579a5bcee5
Barış TÜRE <baristure@hotmail.com>
2021-09-12 21:45:04 +0300

    Add authSlicer registerUser unit tests

--- client/src/__tests__/unit/reduxAuthTest.spec.js
+++ client/src/__tests__/unit/reduxAuthTest.spec.js
@@ -3,11 +3,12 @@ import {
   authReducer,
   loginUser,
   initialState,
+  registerUser,
 } from "../../store/slices/authSlice";
 
 const appState = store.getState();
 describe("authSlice", () => {
-  describe("loginUser State, loginUser Action and loginUser Selector", () => {
+  describe("loginUser State, loginUser Action", () => {
     it("should set fetching state on true when API call is pending", async () => {
       // Arrange
       const action = {
@@ -27,7 +28,10 @@ describe("authSlice", () => {
       // Arrange
       const action = {
         type: loginUser.rejected.type,
-        payload: { code: 401, message: "Incorrect username or password" },
+        payload: {
+          code: 401,
+          message: "Any error message from backend server",
+        },
       };
       // Act
       const nextState = await authReducer(initialState, action);
@@ -36,7 +40,7 @@ describe("authSlice", () => {
       expect(rootState.loginUser.isFetching).not.toBeTruthy();
       expect(rootState.loginUser.isError).toBeTruthy();
       expect(rootState.loginUser.errorMessage).toEqual(
-        "Incorrect username or password"
+        "Any error message from backend server"
       );
     });
 
@@ -67,6 +71,65 @@ describe("authSlice", () => {
         action.payload.user.username
       );
       expect(rootState.loginUser.id).toEqual(action.payload.user.id);
+    });
+  });
+
+  describe("registerUser State, registerUser Action", () => {
+    it("should set fetching state on true when API call is pending", async () => {
+      // Arrange
+      const action = {
+        type: registerUser.pending.type,
+      };
+      // Act
+      const nextState = await authReducer(initialState, action);
+      // Assert
+      const rootState = { ...appState, registerUser: nextState };
+
+      expect(rootState.registerUser.isFetching).toBeTruthy();
+      expect(rootState.registerUser.isError).not.toBeTruthy();
+      expect(rootState.registerUser.errorMessage).toEqual("");
+    });
+
+    it("should set rejected, error and errormessage state on false when API call is rejected", async () => {
+      // Arrange
+      const action = {
+        type: registerUser.rejected.type,
+        payload: {
+          code: 400,
+          message: "Any error message from backend server",
+        },
+      };
+      // Act
+      const nextState = await authReducer(initialState, action);
+      // Assert
+      const rootState = { ...appState, registerUser: nextState };
+      expect(rootState.registerUser.isFetching).not.toBeTruthy();
+      expect(rootState.registerUser.isError).toBeTruthy();
+      expect(rootState.registerUser.errorMessage).toEqual(
+        "Any error message from backend server"
+      );
+    });
+
+    it("should set email, username, id and isSuccess state on false when API call is fulfilled", async () => {
+      // Arrange
+      const action = {
+        type: registerUser.fulfilled.type,
+        payload: {
+          email: "email_value",
+          username: "username_value",
+        },
+      };
+      // Act
+      const nextState = await authReducer(initialState, action);
+      // Assert
+      const rootState = { ...appState, registerUser: nextState };
+
+      expect(rootState.registerUser.isFetching).not.toBeTruthy();
+      expect(rootState.registerUser.isError).not.toBeTruthy();
+      expect(rootState.registerUser.isSuccess).toBeTruthy();
+      expect(rootState.registerUser.email).toEqual(action.payload.email);
+      expect(rootState.registerUser.username).toEqual(action.payload.username);
+      expect(rootState.registerUser.id).toEqual(action.payload.id);
     });
   });
 });
